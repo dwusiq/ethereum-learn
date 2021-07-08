712be9f61c1c1f944ae54fea52176b584ed23ad7
dwusiq <dwusiq@163.com>
2021-07-08 14:21:52 +0800

    新增部分代码

--- web/hd-wallet/src/views/Wallet.vue
+++ web/hd-wallet/src/views/Wallet.vue
@@ -11,34 +11,54 @@
     <div class="outBlock">
       <h1>场景二：校验助记词</h1>
       <textarea class="textarea" v-model="inputMmonicContent" placeholder="embody;father;fashion;lemon;sheriff;gospel;suffer;smoke;finish;bus;way;dizzy" rows="4" cols="30"></textarea>
-      <!-- <ul  v-show="validateMnemonicSuccess">
-        <li>私钥：{{privateKey}}</li>  
-        <li>公钥：{{publicKey}} </li>
-        <li>地址：{{address}}</li>
-        <li>path：{{path}} </li>
-      </ul> -->
-  
-      <ul class="buttonBlock">
-        <button class="button"  @click="validateMnemonic">校验</button>
-        <button class="button"  @click="cleanInputedMnemonic">清空</button>
-        <!-- <button class="button" v-show="validateMnemonicSuccess"  @click="hideValidateMnemonicResult">隐藏</button> -->
-      </ul>
-    </div>
-    <div class="outBlock">
-      <h1>场景三：当前钱包信息</h1>
       <ul  v-show="validateMnemonicSuccess">
+        <h1>当前钱包信息</h1>
         <li>助记词：{{inputMmonicContent}}</li>
         <li>私钥：{{privateKey}}</li>  
         <li>公钥：{{publicKey}} </li>
         <li>地址：{{address}}</li>
         <li>种子：{{seed}}</li>
+        <li>链码：{{chainCode}} </li>
+        <li>扩展键：{{extendedKey}} </li>
         <li>path：{{path}} </li>
       </ul>
-      <!-- <ul class="buttonBlock">
+      <ul class="buttonBlock">
         <button class="button"  @click="validateMnemonic">校验</button>
         <button class="button"  @click="cleanInputedMnemonic">清空</button>
-        <button class="button" v-show="validateMnemonicSuccess"  @click="hideValidateMnemonicResult">隐藏</button>
-      </ul> -->
+      </ul>
+    </div>
+    <div class="outBlock">
+      <h1>场景三：生成子私钥</h1>
+      <ul>
+        <table class="generateSubsidiaryKey">
+          <tr>
+            <td>父私钥初始化参数类型：</td>
+            <td> 
+              <input type="radio" id="mnemonic" value="{{ParentkeyInitTypeEnum.Mnemonic}}" v-model="parentkeyInitType">
+              <label for="mnemonic">助记词</label>
+              <input type="radio" id="seed" value="{{ParentkeyInitTypeEnum.Seed}}" v-model="parentkeyInitType">
+              <label for="seed">随机种子</label>
+              <input type="radio" id="extendedKey" value="ExtendedKey" v-model="parentkeyInitType">
+              <label for="extendedKey">扩展键</label>
+            </td>
+          </tr>
+          <tr>
+             <td>父私钥初始化参数：</td>
+            <td> 
+              <textarea class="textarea" v-model="parentkeyInitParam"  rows="2" cols="40"></textarea>
+            </td>
+          </tr>
+          <tr>
+            <td>子私钥索引：</td>
+            <td> 
+              <input type="text" v-model="subsidiaryKeyIndex" style="width:50px;"/>
+            </td>
+          </tr>
+        </table>
+      </ul>
+      <ul class="buttonBlock">
+        <button class="button"  @click="generateSubsidiaryKey">确认</button>
+      </ul>
     </div>
   </div>
 </template>
@@ -50,11 +70,20 @@ import { ethers } from 'ethers';
 import { defineComponent} from 'vue'
 
 
+enum ParentkeyInitTypeEnum {
+  Mnemonic,
+  Seed,
+  ExtendedKey
+}
+
+
+
 export default defineComponent({
   name: 'WalletPage',
     //用于初始化属性值
     data() {
     return {
+      //主节点属性
       validateMnemonicSuccess:false,
       mnemonicContent: "",
       inputMmonicContent:"",
@@ -62,7 +91,13 @@ export default defineComponent({
       publicKey: "",
       address: "",
       path: "",
-      seed:""
+      seed:"",
+      chainCode:"",
+      extendedKey:"",
+      //其他
+      parentkeyInitType:"Mnemonic",
+      parentkeyInitParam:"",
+      subsidiaryKeyIndex: null
     };
   },
 
@@ -87,6 +122,8 @@ export default defineComponent({
       this.publicKey = hdNode.publicKey;
       this.address = hdNode.address;
       this.path = hdNode.path;
+      this.chainCode = hdNode.chainCode;
+      this.extendedKey  = hdNode.extendedKey ;
       this.seed = ethers.utils.mnemonicToSeed(realMnemonicContent);
     }else{
         alert("请输入有效助记词");
@@ -98,6 +135,9 @@ export default defineComponent({
    },
    hideValidateMnemonicResult():void{
       this.validateMnemonicSuccess=false;
+   },
+   generateSubsidiaryKey():void{
+     console.log(ParentkeyInitTypeEnum.Mnemonic);
    }
   }
 })
@@ -134,5 +174,8 @@ export default defineComponent({
   color:#070202;
   // font-family:黑体;
   font-size:30pt;
+}
+.generateSubsidiaryKey{
+  text-align:left;
 }
 </style>
