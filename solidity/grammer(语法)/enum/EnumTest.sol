88ca5daee7ac92dc19b36c81bf710850f1fb7836
wusiq <dwusiq@163.c0m>
2021-07-27 10:38:22 +0800

    新增枚举`enum`相关案例

---
+++ solidity/grammer(语法)/enum/EnumTest.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+contract EnumTest{
+  enum STATUS_ENUM{START,STOP}
+  
+  STATUS_ENUM public status;
+  
+  uint8 public value;
+
+  constructor(){
+      status=STATUS_ENUM.START;
+  }
+  
+  function change(uint8 _value) requireStart public{
+      value=_value;
+      status=STATUS_ENUM.STOP;
+  }
+  
+  modifier requireStart(){
+      require(status==STATUS_ENUM.START,"require status is start");
+      _;
+  }
+}
---
+++ solidity/grammer(语法)/enum/README.md
@@ -0,0 +1,9 @@
+# `enum`描述
+
+## 概览
+枚举：可以定义一定数量的常量值，构建自定义类型
+
+## 使用
+
+1. 以`enum`开头定义结构体,如：`enum STATUS_ENUM{START,STOP}`
+2. 直接以`EnumName.value`形式调用，如：`STATUS_ENUM status=STATUS_ENUM.START;`
