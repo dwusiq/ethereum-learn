7808a3ff8e60cf524d0f1c09f4abb155e5c83ca5
wusiq <dwusiq@163.c0m>
2021-07-29 09:03:11 +0800

    文件重命名

--- solidity/grammer/contracts/ABIEncoderV2/ABIEncoderV2Demo.sol
+++ solidity/grammer/contracts/ABIEncoderV2/ABIEncoderV2Demo.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
-contract ABIEncoderDemo{
+contract ABIEncoderV2Demo{
     struct User{
         string name;
         uint8 age;
--- solidity/grammer/test/ABIEncoderV2Demo.js
+++ solidity/grammer/test/ABIEncoderV2Demo.js
@@ -11,15 +11,15 @@ const { expect } = require("chai");
          const [Owner,Alice,Bob] = await ethers.getSigners();
  
         //部署ABIEncoderDemo.sol
-         const ABIEncoderDemo = await ethers.getContractFactory("ABIEncoderDemo");
-         const abiEncoderDemo = await ABIEncoderDemo.deploy(userName,userAge);
-         await abiEncoderDemo.deployed();
+         const ABIEncoderV2Demo = await ethers.getContractFactory("ABIEncoderV2Demo");
+         const abiEncoderV2Demo = await ABIEncoderV2Demo.deploy(userName,userAge);
+         await abiEncoderV2Demo.deployed();
  
         //打印合约地址
-         console.log("ABIEncoderDemo address：",abiEncoderDemo.address);
+         console.log("ABIEncoderV2Demo address：",abiEncoderV2Demo.address);
          
          //打印
-         const userInfo = await abiEncoderDemo.getUser();
+         const userInfo = await abiEncoderV2Demo.getUser();
          console.log("userInfo",userInfo);
 
          //校验
