f185ead892bf29521eaf1aa1115d3db2a1e4a71d
dwusiq <dwusiq@163.com>
2021-09-02 18:14:57 +0800

    fix:原本代码混乱了

--- solidity/grammer/test/ReceiveDemo.js
+++ solidity/grammer/test/ReceiveDemo.js
@@ -1,41 +1,40 @@
 const { expect } = require("chai");
- 
- describe("ReceiveDemo test",function (){
 
-     it("deploy ReceiveDemo and test fus();\n" +
-         "\n" +
-         "\n" +
-         "        //测试参数\n" +
-         "        const value=\"10.0\";\n" +
-         "      \n" +
-         "        //部署ReceiveDemo.sol\n" +
-         "         const ReceiveDemo = await ethers.getContractFactory(\"ReceiveDemo\");\n" +
-         "         const receiveDemo = await ReceiveDemo.deploy();nction", async function() {
+describe("ReceiveDemo test", function () {
 
-        const [Alice] = await ethers.getSigner
-         await receiveDemo.deployed();
+    it("deploy ReceiveDemo and test function", async function () {
+        //测试参数
+        const value = "10.0";
+        const [Alice] = await ethers.getSigners();
+
+        //部署ReceiveDemo.sol
+        const ReceiveDemo = await ethers.getContractFactory("ReceiveDemo");
+        const receiveDemo = await ReceiveDemo.deploy();
+
+    
+        await receiveDemo.deployed();
 
         //打印合约地址
-         console.log("receiveDemo address：",receiveDemo.address);
-        
-         //给合约转`eth`,触发`receive`函数
-         const tx = {to: receiveDemo.address,value: ethers.utils.parseEther(value)}
-         const transactionReceipt = await Alice.sendTransaction(tx);
+        console.log("receiveDemo address：", receiveDemo.address);
 
-        receiveDemo.queryFilter(receiveDemo.filters.ReceiveCall(),transactionReceipt.blockNumber,transactionReceipt.blockNumer)
-        .then(rsp => {
-            console.log("ether sender:",rsp[0].args.sender);
-            expect(Alice.address).to.equal(rsp[0].args.sender);
-            console.log("ether amount:",rsp[0].args.amount)
-            expect(ethers.utils.parseEther(value)).to.equal(rsp[0].args.amount);
-        
-        })
-        .catch(err => console.error("error:",err)); 
+        //给合约转`eth`,触发`receive`函数
+        const tx = { to: receiveDemo.address, value: ethers.utils.parseEther(value) }
+        const transactionReceipt = await Alice.sendTransaction(tx);
+
+        receiveDemo.queryFilter(receiveDemo.filters.ReceiveCall(), transactionReceipt.blockNumber, transactionReceipt.blockNumer)
+            .then(rsp => {
+                console.log("ether sender:", rsp[0].args.sender);
+                expect(Alice.address).to.equal(rsp[0].args.sender);
+                console.log("ether amount:", rsp[0].args.amount)
+                expect(ethers.utils.parseEther(value)).to.equal(rsp[0].args.amount);
+
+            })
+            .catch(err => console.error("error:", err));
 
         //查看余额
-        console.log("receiveDemo value:",await receiveDemo.getBalance());
+        console.log("receiveDemo value:", await receiveDemo.getBalance());
         // expect(value).to.equal(await storage.get());
 
 
-     });
- });
+    });
+});
